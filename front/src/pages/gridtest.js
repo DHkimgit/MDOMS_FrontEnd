4fe185a1112aa8667d872cdde897ec464070973f
DHkimgit <dukekim001@naver.com>
2022-09-05 11:14:07 +0000

    09052014

--- front/src/pages/gridtest.js
+++ front/src/pages/gridtest.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import 'tui-grid/dist/tui-grid.css';
 import Grid from '@toast-ui/react-grid';
+import axios from 'axios';
+import { useAsync } from 'react-async';
+
 
 const data = [
     {username: '김두현', id: 1, name: 'Editor'},
@@ -9,17 +12,62 @@ const data = [
   ];
   
 const columns = [
-    {name: 'username', header: 'UserName'},
     {name: 'id', header: 'ID'},
-    {name: 'name', header: 'Name'}
+    {name: 'username', header: 'UserName'},
+    {name: 'servicenumber', header: 'ServiceNumber'},
+    {name: 'email', header: 'Email'},
+    {name: 'affiliatedunit', header: 'AffiliatedUnit'},
   ];
 
+async function getUsers() {
+  const response = await axios.get(
+    'https://mdoms-backend.run.goorm.io/user/'
+  );
+  return response.data.data[0];
+}
+
 function GridTest() {
-    
-      return(
+    const [patchdata, setPatchData] = useState();
+    const [length, setLength] = useState();
+    const [userdata, setUserData] = useState();
+    const data = []
+
+    const fatchdata = async() => {
+      const response = await axios.get('https://mdoms-backend.run.goorm.io/user/')
+      return response.data.data[0]
+    }
+    useEffect(() => {
+      setUserData = fatchdata();
+    }, [])
+    // useEffect(() => {
+    //   axios.get('https://mdoms-backend.run.goorm.io/user/')
+    //   .then(result => {
+    //     setPatchData(result.data.data[0])
+    //     setLength(result.data.data[0].length)
+    //   })
+    // }, []);
+    // for (let i = 0 ; i <= length; i++) {
+    //   let id = patchdata[i].id;
+    //   let username = patchdata[i].UserName;
+    //   let servicenumber = patchdata[i].ServiceNumber;
+    //   let email = patchdata[i].Email;
+    //   let affiliatedunit = patchdata[i].AffiliatedUnit;
+    //   let tmp = {
+    //     id: {id},
+    //     username: {username},
+    //     servicenumber: {servicenumber},
+    //     email: {email},
+    //     affiliatedunit: {affiliatedunit}
+    //   }
+    //   data.push(tmp)
+    // }
+    console.log(userdata)
+    console.log(userdata)
+
+    return(
         <>
             <Grid
-            data={data}
+            data={userdata}
             columns={columns}
             rowHeight={25}
             bodyHeight={100}
@@ -28,7 +76,7 @@ function GridTest() {
             draggable={true}
             />
         </>
-      )
+    )
 }
 
 export default GridTest;
